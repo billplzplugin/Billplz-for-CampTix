8d69ae2f40f4635e80051089b69270cfd0784df6
Wan Zulkarnain <wan@wanzul-hosting.com>
2017-06-27 23:56:42 +0800

    First Release

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# Billplz-for-CampTix
+# Billplz for CampTix
+
+Accept Internet Banking Payment using Billplz
+
+# Installation
+
+0. Ensure CampTix currency is set to **Malaysian Ringgit** (**MYR**)
+1. Download Plugin: https://codeload.github.com/wzul/Billplz-for-CampTix/zip/master
+2. **Upload** & **Install** & **Activate** this plugin to your WordPress
+3. Go to WordPress **Dashboard** >> **Tickets** >> **Setup** >> **Payment** >> **Billplz**
+  * Enabled => **Yes**
+  * **API Secret Key** => Get from **www.billplz.com** and navigate to your account settings
+  * **X Signature Key** => Get from **www.billplz.com** and navigate to your account settings
+  * **Send Bills** => No
+  * **Save Changes**
+4. Done
+
+# Compatibility
+
+1. Compatible with **PHP 5.6/7.0**
+2. WordPress (Self-Hosted) with **CampTix Event Ticketing Plugins**
+
+# Other
+
+Please open an issue or email to wan@wanzul-hosting.com
---
+++ camptix-billplz.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Plugin Name: Billplz for CampTix
+ * Plugin URI: http://www.github.com/wzul/billplz-for-camptix/
+ * Description: Billplz Payment Gateway | Accept Payment using all participating FPX Banking Channels. <a href="https://www.billplz.com/join/8ant7x743awpuaqcxtqufg" target="_blank">Sign up Now</a>.
+ * Author: Wanzul Hosting Enterprise
+ * Author URI: http://www.fb.com/billplzplugin
+ * Version: 3.0
+ * License: GPLv2 or later
+ * Text Doomain: billplz
+ */
+
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+// Add MYR currency
+add_filter( 'camptix_currencies', 'camptix_billplz_add_inr_currency' );
+function camptix_billplz_add_inr_currency( $currencies ) {
+	if ( ! $currencies['MYR'] ) {
+		$currencies['MYR'] = array(
+			'label' => __( 'Ringgit Malaysia', 'billplz' ),
+			'format' => 'RM %s',
+		);
+	}
+	return $currencies;
+}
+
+// Load the Billplz Payment Method
+add_action( 'camptix_load_addons', 'camptix_billplz_load_payment_method' );
+function camptix_billplz_load_payment_method() {
+	if ( ! class_exists( 'CampTix_Payment_Method_Billplz' ) ){
+        require_once plugin_dir_path( __FILE__ ) . 'includes/billplz.php';
+		require_once plugin_dir_path( __FILE__ ) . 'classes/class-camptix-payment-method-billplz.php';
+    }
+	camptix_register_addon( 'CampTix_Payment_Method_Billplz' );
+}
